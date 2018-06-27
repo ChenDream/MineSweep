f73ca050f176cbc023b2e7cfc82cf212fb7badb3
ChenDream <lingchen1991@hotmail.com>
2018-06-27 22:37:47 +0800

    init

---
+++ stable.h
@@ -0,0 +1,10 @@
+#ifndef STABLE_H
+#define STABLE_H
+
+#include <QtCore>
+#include <QWidget>
+#include <QtGui>
+#include <vector>
+
+
+#endif // STABLE_H
---
+++ tools.h
@@ -0,0 +1,12 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+
+class Tools
+{
+public:
+    Tools();
+    static GenerateMap()
+};
+
+#endif // TOOLS_H
