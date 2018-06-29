dc318b8394978e7760b5d3c8ee90b0218ad4de94
ChenDream <lingchen1991@hotmail.com>
2018-06-29 17:54:55 +0800

    day1

--- tools.cpp
+++ tools.cpp
@@ -1,6 +1,11 @@
 #include "tools.h"
 
+
 Tools::Tools()
 {
 
+}
+Matrix Tools::GenerateMap(int width, int length, int mines){
+    Matrix x;
+    return x;
 }
--- tools.h
+++ tools.h
@@ -1,12 +1,13 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
-
+#include <stable.h>
+typedef std::vector<std::vector<int>> Matrix;
 class Tools
 {
 public:
     Tools();
-    static GenerateMap()
+    static Matrix GenerateMap(int width,int length,int mines);
 };
 
 #endif // TOOLS_H
